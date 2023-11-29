a18aba98ffcb6776539b21a509d2066a5d4adee1
surya <suryasarisa99@gmail.com>
2023-11-29 16:51:24 +0530

    now we can add items

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState, useRef } from "react";
-import { FaPlus } from "react-icons/fa";
+import { FaChevronLeft, FaCross, FaPlus } from "react-icons/fa";
 import { FiPlus } from "react-icons/fi";
+import { MdClose } from "react-icons/md";
 function App() {
   const [payments, setPayments] = useState(
     JSON.parse(localStorage.getItem("payments")) || [
@@ -13,6 +14,7 @@ function App() {
       { type: "other", money: 2000, description: "money added" },
     ]
   );
+  const [create, setCreate] = useState(false);
   const [selected, setSelected] = useState(0);
   const [add, setAdd] = useState("");
   const inputRef = useRef(null);
@@ -31,6 +33,30 @@ function App() {
     localStorage.setItem("payments", JSON.stringify(payments));
   }, [payments]);
 
+  function handleFormSubmit(e, p_index) {
+    e.preventDefault();
+    payments[p_index].profit += +add;
+    setPayments([...payments]);
+    setSelected(-1);
+    setAdd("");
+  }
+  function onPlusClick(e, p_index) {
+    if (selected != p_index) {
+      setSelected(p_index);
+    } else {
+      if (!add) {
+        setSelected(-1);
+      } else {
+        payments[p_index].profit += +add;
+        setPayments([...payments]);
+        setAdd("");
+      }
+    }
+  }
+  function addRecord(record) {
+    setPayments((p) => [...p, record]);
+  }
+
   const [total, setTotal] = useState(0);
   return (
     <div className="payments">
@@ -44,36 +70,10 @@ function App() {
               <p className="invested">{payment.invested}</p>
               <FiPlus
                 className="add-btn"
-                onClick={(e) => {
-                  if (selected != p_index) {
-                    setSelected(p_index);
-                    // setTimeout(() => {
-                    //   const temp =
-                    //     document.querySelectorAll(".payment")[p_index];
-                    //   temp.querySelector(".input").focus();
-                    // }, 1000);
-                  } else {
-                    if (!add) {
-                      setSelected(-1);
-                    } else {
-                      payments[p_index].profit += +add;
-                      setPayments([...payments]);
-                      setAdd("");
-                    }
-                  }
-                }}
+                onClick={(e) => onPlusClick(e, p_index)}
               />
               {selected == p_index ? (
-                <form
-                  action=""
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    payments[p_index].profit += +add;
-                    setPayments([...payments]);
-                    setSelected(-1);
-                    setAdd("");
-                  }}
-                >
+                <form action="" onSubmit={(e) => handleFormSubmit(e, p_index)}>
                   <input
                     type="text"
                     value={add}
@@ -97,12 +97,102 @@ function App() {
         }
       })}
 
+      {create && (
+        <Create addRecord={addRecord} onClose={() => setCreate(false)} />
+      )}
+
       <p className="total">
         Total:
         <span className="total-value">{total}</span>
+        {!create && (
+          <div className="add-btn-outer">
+            <FiPlus className="add-btn" onClick={(e) => setCreate(true)} />
+          </div>
+        )}
       </p>
     </div>
   );
+}
+
+function Create({ addRecord, onClose }) {
+  const [type, setType] = useState("invxest");
+  const [invest, setInvest] = useState("");
+  const [profit, setProfit] = useState("");
+
+  useEffect(() => {
+    setProfit(-1 * invest || "");
+  }, [invest]);
+
+  const Buttons = (
+    <div className="btns">
+      <div className="left">
+        <FaChevronLeft onClick={(e) => setType("x")} />
+        <MdClose onClick={onClose} />
+      </div>
+      <div className="right">
+        <button className="submit">Submit</button>
+      </div>
+    </div>
+  );
+
+  if (type == "invest")
+    return (
+      <form
+        className="create"
+        onSubmit={(e) => {
+          e.preventDefault();
+          addRecord({
+            type: "invest",
+            profit,
+            invested: +invest,
+            name: e.target.name.value,
+          });
+          onClose();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="invest"
+          value={invest}
+          onChange={(e) => setInvest(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="profit"
+          value={profit}
+          onChange={(e) => setProfit(e.target.value)}
+        />
+        <input type="text" placeholder="name" name="name" />
+        {Buttons}
+      </form>
+    );
+  else if (type == "money")
+    return (
+      <form
+        action=""
+        className="create"
+        onSubmit={(e) => {
+          e.preventDefault();
+          addRecord({
+            type: "other",
+            money: +e.target.money.value,
+            description: e.target.description.value,
+          });
+          onClose();
+        }}
+      >
+        <input type="text" name="money" placeholder="money" />
+        <input type="text" name="description" placeholder="description" />
+        {Buttons}
+      </form>
+    );
+  else
+    return (
+      <div className="create">
+        <button onClick={() => setType("invest")}>Invest</button>
+        <button onClick={() => setType("money")}>Add Money</button>
+      </div>
+    );
 }
 
 export default App;
